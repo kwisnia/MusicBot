9adb6b3fe3f072b5c31719de177927091555003e
kwisnia <230037@edu.p.lodz.pl>
2021-10-03 22:36:24 +0200

    Add queue command

---
+++ src/commands/QueueCommand.ts
@@ -0,0 +1,104 @@
+/* eslint-disable import/order */
+import { SlashCommandBuilder } from '@discordjs/builders';
+import {
+  Client,
+  CommandInteraction,
+  GuildMember,
+  Message,
+  MessageComponentInteraction,
+  MessageEmbed,
+  VoiceChannel,
+} from 'discord.js';
+import { Logger } from 'winston';
+import NoGuildError from '../errors/NoGuildError';
+import UserNotInVoiceChannelError from '../errors/UserNotInVoiceChannelError';
+import { ISubscriptionService } from '../services/music/ISubscriptionService';
+import BaseCommand from '../services/interaction/BaseCommand';
+
+export default class QueueCommand extends BaseCommand {
+  public readonly data;
+
+  public constructor(
+    protected logger: Logger,
+    protected subscriptionService: ISubscriptionService,
+    protected client: Client,
+  ) {
+    super(logger, subscriptionService, client);
+    this.data = new SlashCommandBuilder()
+      .setName('queue')
+      .setDescription('Gives information about queue in current server');
+  }
+
+  public async execute(
+    interaction: CommandInteraction,
+  ): Promise<Message | undefined | void> {
+    this.logger.info('Queue command called');
+    if (!(interaction.member instanceof GuildMember)) {
+      throw new NoGuildError();
+    }
+    const { channel } = interaction.member.voice;
+    if (!channel || !(channel instanceof VoiceChannel)) {
+      throw new UserNotInVoiceChannelError(
+        'Command queue was not called in a voice channel',
+      );
+    }
+    const currentStatus = await this.subscriptionService.getSubscriptionStatus(
+      interaction.guildId!,
+    );
+    let embed;
+    if (currentStatus.currentTrack) {
+      embed = new MessageEmbed()
+        .setColor('#00FF00')
+        .setTitle('Playback queue')
+        .addField('Currently playing', currentStatus.currentTrack.title, true)
+        .setThumbnail(currentStatus.currentTrack.thumbnailUrl)
+        .addFields(
+          {
+            name: 'Requested by',
+            value:
+              this.client.users.cache.get(
+                currentStatus.currentTrack.requestedBy,
+              )?.username || 'unknown',
+            inline: true,
+          },
+          { name: '\u200B', value: '\u200B' },
+          {
+            name: 'Queue',
+            value: currentStatus.queue.length
+              ? currentStatus.queue
+                  .map(
+                    (track, index) =>
+                      `${index + 1}. ${track.title} (requested by: ${
+                        this.client.users.cache.get(track.requestedBy)
+                          ?.username || 'unknown'
+                      })`,
+                  )
+                  .join('\n')
+              : 'Queue is empty 😥 Add some songs!',
+          },
+        )
+        .setFooter(
+          `Loop: ${
+            currentStatus.loopSingle ? 'enabled' : 'disabled'
+          }\nShuffle: ${currentStatus.shuffle ? 'enabled' : 'disabled'}`,
+        );
+    } else {
+      embed = new MessageEmbed()
+        .setColor('#880808')
+        .setTitle('❌')
+        .setDescription('Nothing is playing');
+    }
+    await interaction.reply({
+      embeds: [embed],
+      ephemeral: true,
+    });
+    return Promise.resolve();
+  }
+
+  public async handleComponentInteraction(
+    interaction: MessageComponentInteraction,
+  ): Promise<Message | undefined | void> {
+    this.logger.info(`Play component clicked by ${interaction.user.id}`);
+    return Promise.resolve();
+  }
+}
